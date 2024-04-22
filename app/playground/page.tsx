d3772b24fec2f56dcfbd2f5a42f7e05f66dbddb3
“Vijeet <vijeetbshah@gmail.com>
2024-04-22 23:08:44 +0530

    add output preview browser iframe

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -2,12 +2,17 @@
 
 import CodeEditor from "@/components/codeEditor";
 import FileSelector from "@/components/fileStructure";
+import OutputBrowser from "@/components/outputBrowser";
 import XtermTerminal from "@/components/terminal";
 import React, { useState } from "react";
 
 const PlayGround = () => {
   const [files, setFiles] = useState([
-    { name: "index.html", language: "html", content: "" },
+    {
+      name: "index.html",
+      language: "html",
+      content: "<h1>Hello Vijeet Shah<h1>",
+    },
     { name: "style.css", language: "css", content: "" },
     { name: "script.js", language: "javascript", content: "" },
   ]);
@@ -17,6 +22,22 @@ const PlayGround = () => {
   const handleFileClick = (file: (typeof files)[0]) => {
     setSelectedFile(file);
   };
+
+  const htmlFile = files.find((file) => file.language === "html");
+  const cssFile = files.find((file) => file.language === "css");
+  const jsFile = files.find((file) => file.language === "javascript");
+
+  const html = `
+  <html>
+    <head>
+      <style>${cssFile?.content}</style>
+    </head>
+    <body>
+      ${htmlFile?.content}
+      <script>${jsFile?.content}</script>
+    </body>
+  </html>
+`;
 
   return (
     <div>
@@ -29,6 +50,8 @@ const PlayGround = () => {
       <CodeEditor />
 
       <XtermTerminal />
+
+      <OutputBrowser html={html} />
     </div>
   );
 };
